f3be4534f36d8514343dbf5924ad120d0e627f33
apazze <apazze@users.noreply.github.com>
2022-04-29 09:11:47 -0300

    Adicionando usos de Factory method, conceito de fluxo reativo e inferência de variável.

--- src/br/com/alura/java/novidades/Testes.java
+++ src/br/com/alura/java/novidades/Testes.java
@@ -1,7 +1,33 @@
 package br.com.alura.java.novidades;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.SubmissionPublisher;
+
 public class Testes {
     public static void main(String[] args) {
         System.out.println("(Java Moderno > Java 8) == true");
+        //Factory method
+        List<String> nomes = List.of("nome1", "nome2", "nome3");
+        System.out.println(nomes);
+
+        //Fluxo reativo (exemplo aplicação de NFs)
+        //Antes - enviava as NFs descontroladamente para o webservice se virar gerando gargalos, com riso de perdas ou não envios
+        // Agora - o webservice que solicita a qtd de NFs que pode processar
+
+        /*NotaFiscalSubscriver nfs = new NotaFiscalSubscriber();
+        SubmissionPublisher<NotaFiscal> publisher = new SubmissionPublisher<NotaFiscal>();
+        publisher.subscribe(nfs);
+
+        NotaFiscal notaFiscal = new NotaFiscal("João", 39.99, LocalDate.now());
+        publisher.submit(notaFiscal);*/
+
+        //variavel local (escopo de método)
+        // o tipo da referencia fica implícito, o Java vai inferir o tipo em tempo de execução
+        var inferenciaDeVariavel = new ArrayList<String>();
+        inferenciaDeVariavel.add("Joao");
+        System.out.println(inferenciaDeVariavel);
+
     }
 }
